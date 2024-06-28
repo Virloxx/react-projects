140efec74472dd42be8d05b7dc76a4912551fd11
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-06-28 02:03:21 +0200

    resize image and add container

--- digital-business-card/src/components/Info.jsx
+++ digital-business-card/src/components/Info.jsx
@@ -3,7 +3,7 @@ import './Info.css';
 export default function Info() {
     return (
         <div className="info">
-            <img src="src/assets/Me.JPEG" alt="Picture of me"/>
+            <img src="src/assets/Me.JPEG" alt="Picture of me" />
             <h1>Oskar Pankowski</h1>
             <h3>Student</h3>
             <p>oskarpankowski.website</p>
