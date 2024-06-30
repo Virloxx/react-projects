975b0a911d1622c57beca6c854899fc16dcf33c1
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-06-30 17:13:53 +0200

    add navbar styling

--- travel-journal/src/components/Navbar.jsx
+++ travel-journal/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import './Navbar.css';
+import worldIcon from '../assets/world-icon.svg'
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img src="src/assets/world-icon.svg" alt="World Icon" />
-            <h1>my travel journal.</h1>
+            <img src={worldIcon} alt="World Icon" className="world-icon"/>
+            <h1 className="navbar--title">my travel journal.</h1>
         </nav>
     )
 }
