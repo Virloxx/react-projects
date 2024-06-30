ba862d3bdeea4dcda48149a316d64bc5c137987c
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-06-30 14:43:30 +0200

    add navbar component

---
+++ travel-journal/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import './Navbar.css';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img src="src/assets/world-icon.svg" alt="World Icon" />
+            <h1>my travel journal.</h1>
+        </nav>
+    )
+}
