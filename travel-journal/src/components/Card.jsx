4880f15e293063b1ef269beec587250902f4a8d6
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-07-01 02:11:01 +0200

    add card styling

--- travel-journal/src/components/Card.jsx
+++ travel-journal/src/components/Card.jsx
@@ -4,14 +4,19 @@ export default function Card(props) {
     return (
         <div className="card">
             <img src={props.imageUrl} alt={`${props.title} Image`} className="card--image" />
-            <div className="location">
-                <img src="src/assets/pin-icon.svg" alt="Pin Icon" />
-                <h2>{props.location.toUpperCase()}</h2>
-                <a href={props.googleMapsUrl} target="_blank" rel="noopener noreferrer">View on Google Maps</a>
-            </div>
-            <h1>{props.title}</h1>
-            <p className="card--date">{props.startDate} - {props.endDate}</p>
-            <p className="description">{props.description}</p>        
+            <div className="card--text">
+                <div className="location">
+                    <img src="src/assets/pin-icon.svg" alt="Pin Icon" className="pin--icon"/>
+                    <h2 className="country">{props.location.toUpperCase()}</h2>
+                    <a href={props.googleMapsUrl}
+                       target="_blank" 
+                       rel="noopener noreferrer" 
+                       className="location--link">View on Google Maps</a>
+                </div>
+                <h1 className="card--title">{props.title}</h1>
+                <p className="card--date">{props.startDate} - {props.endDate}</p>
+                <p className="description">{props.description}</p>
+            </div> 
         </div>
     )
 }
