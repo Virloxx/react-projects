33a3cabf5bd98008a546e12ef35614e2794dcfda
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-07-03 03:01:32 +0200

    store cards as a state variable
    add dummy card on button click

--- interactive-travel-journal/src/App.jsx
+++ interactive-travel-journal/src/App.jsx
@@ -2,23 +2,41 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Card from './components/Card'
 import data from './data'
+import { useState } from 'react'
 
 function App() {
-  const cards = data.map((card) => {
+  const [cards, setCards] = useState(data)
+
+  const cardElements = cards.map((card) => {
     return (
       <div className="card--container">
-            <Card 
-              {...card}
-            />
-            <hr className="card--spacer"/>
+        <Card
+          {...card}
+        />
+        <hr className="card--spacer"/>
       </div>
     )
   })
-  
+
+  function addCard() {
+    const newCard = {
+      title: "Mount Fuji",
+      location: "Japan",
+      googleMapsUrl: "https://maps.app.goo.gl/aC4XKC68csaxVpa38",
+      startDate: "12 Jan 2021",
+      endDate: "24 Jan 2021",
+      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      imageUrl: "https://images.unsplash.com/photo-1564083573637-ec42bdf05148?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+    setCards(prevState => [...prevState, newCard])
+  }
+
   return (
     <>
-      <Navbar />
-      {cards}
+      <Navbar 
+        handleClick={addCard}
+      />
+      {cardElements}
     </>
   )
 }
--- interactive-travel-journal/src/components/Navbar.jsx
+++ interactive-travel-journal/src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import './Navbar.css';
 import worldIcon from '../assets/world-icon.svg'
 import plusIcon from '../assets/plus-icon.svg'
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
         <nav className="navbar">
             <div className="navbar--logo">
                 <img src={worldIcon} alt="World Icon" className="world-icon"/>
                 <h1 className="navbar--title">my travel journal.</h1>
             </div>
-            <button className="navbar--button" type="button">
+            <button onClick={props.handleClick} className="navbar--button" type="button">
                 <img src={plusIcon} alt="Plus Icon" className="plus-icon"/>
             </button>
         </nav>
