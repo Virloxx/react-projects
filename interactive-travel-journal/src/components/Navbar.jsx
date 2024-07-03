0f3bd4a2fb5b8aed4fbf8d698ef92da8c5c5b880
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-07-03 02:32:59 +0200

    add button to navbar

--- interactive-travel-journal/src/components/Navbar.jsx
+++ interactive-travel-journal/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import './Navbar.css';
 import worldIcon from '../assets/world-icon.svg'
+import plusIcon from '../assets/plus-icon.svg'
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img src={worldIcon} alt="World Icon" className="world-icon"/>
-            <h1 className="navbar--title">my travel journal.</h1>
+            <div className="navbar--logo">
+                <img src={worldIcon} alt="World Icon" className="world-icon"/>
+                <h1 className="navbar--title">my travel journal.</h1>
+            </div>
+            <button className="navbar--button" type="button">
+                <img src={plusIcon} alt="Plus Icon" className="plus-icon"/>
+            </button>
         </nav>
     )
 }
