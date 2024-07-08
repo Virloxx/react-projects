723b66c14a8279e9141a3ec294933459a69f8b67
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-07-08 16:51:56 +0200

    add popup component

--- interactive-travel-journal/src/components/Popup.jsx
+++ interactive-travel-journal/src/components/Popup.jsx
@@ -1,10 +1,13 @@
 import './Popup.css';
+import closeIcon from '../assets/close-icon.svg'
 
 export default function Popup(props) {
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup--inner">
-                <button className="popup--close" onClick={props.close}>X</button>
+                <button className="popup--close" onClick={props.close}>
+                    <img src={closeIcon} alt="Close Icon" className="close--icon" />
+                </button>
                 {props.children}
             </div>
         </div>
