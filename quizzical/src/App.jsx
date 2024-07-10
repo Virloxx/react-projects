78f0fc76b08303e29d39770448041ffc22829b47
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-07-11 01:34:45 +0200

    fetch data and render it to screen
    delete React.StrictMode from main.jsx

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Question from './components/Question'
 
 function App() {
+  const [allQuestions, setAllQuestions] = useState([])
+
+  useEffect(() => {
+    async function getQuestions() {
+      const response = await fetch("https://opentdb.com/api.php?amount=5")
+      const data = await response.json()
+      setAllQuestions(data.results)
+    }
+    getQuestions()
+  }, [])
+
+  const questionElements = allQuestions.map((question, index) => (
+    <Question
+      key={index}
+      question={question.question}
+      answers={question.incorrect_answers.concat(question.correct_answer)}
+    />
+  ))
+
   return (
     <>
-      <h1>Sample Text</h1>
+      {questionElements}
     </>
   )
 }
---
+++ quizzical/src/components/Question.jsx
@@ -0,0 +1,24 @@
+export default function Question(props) {
+    const answers = props.answers.map((answer, index) => (
+            <div key={index}>
+                <input
+                    type="radio"
+                    id={answer}
+                    name="employment"
+                    value={answer}
+                />
+                <label htmlFor={answer}>{answer}</label>
+            </div>
+        )
+    )
+
+    return (
+        <div>
+            <h2>{props.question}</h2>
+            <form>
+                {answers}
+            </form>
+            <hr />
+        </div>
+    );
+}
